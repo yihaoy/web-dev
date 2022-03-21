6da16089b3a1c9b669ec9d2700eeab45f9c8de5e
yihaoy <yihaoyang126@gmail.com>
2022-03-21 03:31:38 -0400

    Update HomeScreen.js

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -3,21 +3,21 @@ import NavigationSidebar from "../NavigationSidebar";
 import PostSummaryList from "../PostSummaryList";
 
 (function ($) {
-    $('#wd-home').append(`
+  $('#wd-home').append(`
        
     <div class="container">
         <div class="row mt-2">
-           <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-3 col-3 mt-1">
+           <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar('home')}
             </div>      
             
-            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-10 col-sm-9 col-9 mt-1">
+            <div class="col-10 col-md-10 col-lg-7 col-xl-6">
                 ${PostList()}
             </div>  
-            <div class="col-xxl-4 col-xl-4 col-lg-4 d-none d-lg-block mt-1">
+            <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
                 ${PostSummaryList()}
             </div>
         </div>
     </div>
-        
     `);
+})($);
