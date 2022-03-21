0196709e21c450f2f5292a32cbfd23206bc3c1b5
yihaoy <yihaoyang126@gmail.com>
2022-03-21 03:50:58 -0400

    Update HomeScreen.js

--- public/tuiter/HomeScreen/HomeScreen.js
+++ public/tuiter/HomeScreen/HomeScreen.js
@@ -1,24 +1,24 @@
-import PostList from "../PostList/PostList.js";
-import NavigationSidebar from "../NavigationSidebar";
-import PostSummaryList from "../PostSummaryList";
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import ExploreComponent from "../ExploreScreen/ExploreComponent.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
+
 
 (function ($) {
-  $('#home').append(`
-       
+  $('#HOME').append(`
     <div class="container">
         <div class="row mt-2">
-           <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-                ${NavigationSidebar('home')}
-            </div>      
-            
-            <div class="col-10 col-md-10 col-lg-7 col-xl-6">
-                ${PostList()}
+            <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-3 col-3 mt-1">
+                ${NavigationSidebar('explore')}
+            </div>
+            <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-10 col-sm-9 col-9 mt-1">
+                ${ExploreComponent()}
             </div>
             
-            <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-                ${PostSummaryList()}
+            <div class="col-xxl-4 col-xl-4 col-lg-4 d-none d-lg-block mt-1">
+                ${WhoToFollowList()}
             </div>
         </div>
     </div>
     `);
 })($);
+
