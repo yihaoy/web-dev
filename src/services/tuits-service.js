d02dc5247e6b7a748c5c1fd4d34d07ea4b66d4fe
yihaoy <yihaoyang126@gmail.com>
2022-04-13 13:09:33 -0400

    a8 Rest sp22

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const TUITS_API = 'https://full-stack-developer-server-yy.herokuapp.com/api/tuits';
 
 //const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
-const API_BASE = process.env.REACT_APP_API_BASE || 'https://full-stack-developer-server-yy.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE || 'https://full-stack-developer-server-yy.herokuapp.com/api';
 
 const TUITS_API = `${API_BASE}/tuits`;
 
